f20b532b2acec0de9e9c327d4a85607e9403625b
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:30:56 +0530

    Create Member-after-test.js

---
+++ Car-auction-network/Test/Participants/Member-after-test.js
@@ -0,0 +1,22 @@
+{
+  "$class": "org.acme.vehicle.auction.Member",
+  "balance": -6000,
+  "email": "4872",
+  "firstName": "Alice",
+  "lastName": "Smith"
+}
+
+{
+  "$class": "org.acme.vehicle.auction.Member",
+  "balance": 6000,
+  "email": "9314",
+  "firstName": "Charlie",
+  "lastName": "Brown"
+}
+{
+  "$class": "org.acme.vehicle.auction.Member",
+  "balance": 0,
+  "email": "9794",
+  "firstName": "Bob",
+  "lastName": "Jones"
+}
