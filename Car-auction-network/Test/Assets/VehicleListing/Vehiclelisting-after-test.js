a505dbb1b46e36d6973505f2bb50ebcb9611fb7d
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:34:59 +0530

    Create Vehiclelisting-after-test.js

---
+++ Car-auction-network/Test/Assets/VehicleListing/Vehiclelisting-after-test.js
@@ -0,0 +1,9 @@
+
+{
+  "$class": "org.acme.vehicle.auction.VehicleListing",
+  "listingId": "listing1",
+  "reservePrice": 0,
+  "description": "",
+  "state": "SOLD",
+  "vehicle": "resource:org.acme.vehicle.auction.Vehicle#1234"
+}
