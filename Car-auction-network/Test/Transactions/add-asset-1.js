f6b0a77cce3829f305327ff32d2a9d5ff620d22b
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:40:21 +0530

    Create add-asset-1.js

---
+++ Car-auction-network/Test/Transactions/add-asset-1.js
@@ -0,0 +1,13 @@
+{
+ "$class": "org.hyperledger.composer.system.AddAsset",
+ "resources": [
+  {
+   "$class": "org.acme.vehicle.auction.Vehicle",
+   "vin": "1234",
+   "owner": "resource:org.acme.vehicle.auction.Member#9314"
+  }
+ ],
+ "targetRegistry": "resource:org.hyperledger.composer.system.AssetRegistry#org.acme.vehicle.auction.Vehicle",
+ "transactionId": "0c7a85c2-02b2-499a-be76-a744fd00ef4c",
+ "timestamp": "2018-12-19T18:51:52.759Z"
+}
