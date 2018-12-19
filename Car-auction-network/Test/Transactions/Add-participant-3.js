d1bf71a2b2cd6172c1267250bfded26dbb68b036
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:39:50 +0530

    Create Add-participant-3.js

---
+++ Car-auction-network/Test/Transactions/Add-participant-3.js
@@ -0,0 +1,15 @@
+{
+ "$class": "org.hyperledger.composer.system.AddParticipant",
+ "resources": [
+  {
+   "$class": "org.acme.vehicle.auction.Member",
+   "balance": 0,
+   "email": "9314",
+   "firstName": "Charlie",
+   "lastName": "Brown"
+  }
+ ],
+ "targetRegistry": "resource:org.hyperledger.composer.system.ParticipantRegistry#org.acme.vehicle.auction.Member",
+ "transactionId": "a5715249-12e3-4ed9-95f4-f06c18d49c11",
+ "timestamp": "2018-12-19T18:51:34.010Z"
+}
