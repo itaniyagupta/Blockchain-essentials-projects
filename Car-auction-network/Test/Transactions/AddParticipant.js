ee58eff4a0d08bf6af2bbb30965d5df725e532cd
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:36:07 +0530

    Create AddParticipant.js

---
+++ Car-auction-network/Test/Transactions/AddParticipant.js
@@ -0,0 +1,12 @@
+{
+ "$class": "org.hyperledger.composer.system.AddParticipant",
+ "resources": [
+  {
+   "$class": "org.hyperledger.composer.system.NetworkAdmin",
+   "participantId": "admin"
+  }
+ ],
+ "targetRegistry": "resource:org.hyperledger.composer.system.ParticipantRegistry#org.hyperledger.composer.system.NetworkAdmin",
+ "transactionId": "5b129d64-1caa-49cf-80a0-d694f70c9134#0",
+ "timestamp": "2018-12-19T18:44:22.404Z"
+}
