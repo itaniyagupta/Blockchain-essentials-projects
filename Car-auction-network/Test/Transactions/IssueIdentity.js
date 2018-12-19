3139afa9d974787965fb7605d0a6cced2d1b1206
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:36:34 +0530

    Create IssueIdentity.js

---
+++ Car-auction-network/Test/Transactions/IssueIdentity.js
@@ -0,0 +1,7 @@
+{
+ "$class": "org.hyperledger.composer.system.IssueIdentity",
+ "participant": "resource:org.hyperledger.composer.system.NetworkAdmin#admin",
+ "identityName": "admin",
+ "transactionId": "5b129d64-1caa-49cf-80a0-d694f70c9134#1",
+ "timestamp": "2018-12-19T18:44:22.405Z"
+}
