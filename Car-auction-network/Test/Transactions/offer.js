28a4e938d29edba61d3724c3153b874e96b75a19
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:41:27 +0530

    Create offer.js

---
+++ Car-auction-network/Test/Transactions/offer.js
@@ -0,0 +1,8 @@
+{
+ "$class": "org.acme.vehicle.auction.Offer",
+ "bidPrice": 6000,
+ "listing": "resource:org.acme.vehicle.auction.VehicleListing#listing1",
+ "member": "resource:org.acme.vehicle.auction.Member#4872",
+ "transactionId": "feb29192-6060-4f3e-839b-e5291ce836e9",
+ "timestamp": "2018-12-19T18:56:32.731Z"
+}
