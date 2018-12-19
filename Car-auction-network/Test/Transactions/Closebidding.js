5337d3e6e6eceff8b595769ed1109930866405bf
TANIYA GUPTA <thetaniyagupta@gmail.com>
2018-12-20 01:41:47 +0530

    Create Closebidding.js

---
+++ Car-auction-network/Test/Transactions/Closebidding.js
@@ -0,0 +1,6 @@
+{
+ "$class": "org.acme.vehicle.auction.CloseBidding",
+ "listing": "resource:org.acme.vehicle.auction.VehicleListing#listing1",
+ "transactionId": "6d229d5f-bec6-451e-9d47-f6f138baa46e",
+ "timestamp": "2018-12-19T18:58:07.871Z"
+}
